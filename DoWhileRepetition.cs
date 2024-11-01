57e2e37bf9fbc1fe81e9ffb97533f51f35cc6291
Anneshu Nag <132702983+NK-Works@users.noreply.github.com>
2024-11-02 00:19:26 +0530

    do while added

---
+++ DoWhileRepetition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DoWhileRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            double average;
+            
+            // Asking for the upperbound number
+            Console.Write("Please enter the upperbound number (as a integer): ");
+            int upperbound = Convert.ToInt32(Console.ReadLine());
+            
+            // Declaring and initialising the current iteration number
+            int number = 1;
+
+            // Using the loop to do the calculation
+            do
+            {
+                sum += number;
+                number++;
+                Console.WriteLine("Current number: " + number + " the sum is " + sum);
+            }while (number <= upperbound);
+
+            // Printing the sum
+            Console.WriteLine("The sum is " + sum);
+            
+            // Printing the average
+            average = (double)sum/upperbound;
+            Console.WriteLine("The average is " + average);
+            
+            Console.ReadLine();
+        }
+    }
+}
